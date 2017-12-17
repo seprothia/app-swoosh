580d838e9dc8988a01f1eaf5582f087182ac8c4b
Benjamin Wilson <Seprothia@Benjamins-MacBook-Pro.local>
2017-12-17 14:06:52 +0000

    Auto layout applied

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Let's position things. This is doing it programmatically. But this won't refresh the layout when you rotate...lots of manual work here!
+        
+        // Getting the horizontal centre of the current view/frame to position swoosh, putting it 50 from the top, keeping width and height of swoosh same
+//        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+//
+//        // Make the image the same size and position as the view
+//        bgImg.frame = view.frame
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
